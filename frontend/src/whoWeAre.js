33df2cefb717dab6b155c357e94379cfe141edbc
Kalinka5 <dania003d@gmail.com>
2024-08-14 12:28:41 +0300

    Changed text on each pages

--- frontend/src/whoWeAre.js
+++ frontend/src/whoWeAre.js
@@ -94,38 +94,7 @@ function WhoWeAre() {
                 <p className="text-white px-12 text-center font-semibold">
                   Основатель
                 </p>
-                <div className="flex gap-2 cursor-pointer justify-center py-2 text-slate-800">
-                  <a href="https://www.linkedin.com/in/sam-magnet">
-                    <svg
-                      stroke="currentColor"
-                      fill="currentColor"
-                      strokeWidth="0"
-                      viewBox="0 0 448 512"
-                      className="bg-[#fca214] rounded"
-                      height="25"
-                      width="25"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path>
-                    </svg>
-                  </a>
-                  <a href="mailto:sam@magnetrecruit.com">
-                    <svg
-                      stroke="currentColor"
-                      fill="currentColor"
-                      strokeWidth="0"
-                      viewBox="0 0 20 20"
-                      aria-hidden="true"
-                      className="bg-[#fca214] rounded"
-                      height="25"
-                      width="25"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
-                      <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
-                    </svg>
-                  </a>
-                </div>
+                <div className="flex gap-2 cursor-pointer justify-center py-2 text-slate-800"></div>
                 <div className="md:w-full px-2">
                   <div className="flex border md:border-none sm:mx-2">
                     <p className="md:w-[25%] w-full text-sm text-[#FCA311] border p-4 md:border-b-0">
@@ -137,7 +106,7 @@ function WhoWeAre() {
                     <p className="p-4 border text-white border-t-transparent text-sm text-justify">
                       Ранее работая в различных компаниях в качестве временного
                       консультанта по подбору персонала, Сэм решил отказаться от
-                      своего статуса сотрудника и создать честную организацию,
+                      своего статуса сотрудника и создать частную организацию,
                       основанную на ценностях, в которые он верит. Он основал
                       Magnet Recruit в разгар пандемии 2020 года с целью
                       создания продвижение культуры в сфере подбора персонала,
@@ -180,38 +149,7 @@ function WhoWeAre() {
                 <p className="text-white px-12 text-center font-semibold">
                   Директор
                 </p>
-                <div className="flex gap-2 cursor-pointer justify-center py-2 text-slate-800">
-                  <a href="https://www.linkedin.com/in/dominic-jones">
-                    <svg
-                      stroke="currentColor"
-                      fill="currentColor"
-                      strokeWidth="0"
-                      viewBox="0 0 448 512"
-                      className="bg-[#fca214] rounded"
-                      height="25"
-                      width="25"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path>
-                    </svg>
-                  </a>
-                  <a href="mailto:dominic@magnetrecruit.com">
-                    <svg
-                      stroke="currentColor"
-                      fill="currentColor"
-                      strokeWidth="0"
-                      viewBox="0 0 20 20"
-                      aria-hidden="true"
-                      className="bg-[#fca214] rounded"
-                      height="25"
-                      width="25"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
-                      <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
-                    </svg>
-                  </a>
-                </div>
+                <div className="flex gap-2 cursor-pointer justify-center py-2 text-slate-800"></div>
                 <div className="md:w-full px-2">
                   <div className="flex border md:border-none sm:mx-2">
                     <p className="md:w-[30%] w-full text-sm text-[#FCA311] border p-4 md:border-b-0">
@@ -227,8 +165,8 @@ function WhoWeAre() {
                       он начал в сфере кадрового менеджмента, накопив
                       значительный опыт в подборе персонала для международных
                       компаний. Стремясь помочь кандидатам найти работу за
-                      границей, Ильджас создал Magnet Recruit, которая стала
-                      успешной благодаря его глубокому пониманию рынка и
+                      границей, Ильджас создал компанию Magnet Recruit, которая
+                      стала успешной благодаря его глубокому пониманию рынка и
                       уникальному подходу к подбору кадров. В своей компании
                       Ильджас лично курирует ключевые процессы, помогая
                       русскоязычным специалистам найти подходящие вакансии и
@@ -423,7 +361,7 @@ function WhoWeAre() {
               <div className="grid w-[25%]"></div>
               <div className="grid w-[50%] bg-[#14213d] pt-24">
                 <h1 className="md:text-6xl text-xl w-full text-[#FCA311] text-center">
-                  Наша суть подбора персонала
+                  Наша философия подбора персонала:
                 </h1>
                 <div className="flex flex-col py-6 md:ml-9">
                   <div className="flex py-1">
@@ -440,13 +378,13 @@ function WhoWeAre() {
                         <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm193.5 301.7l-210.6 292a31.8 31.8 0 0 1-51.7 0L318.5 484.9c-3.8-5.3 0-12.7 6.5-12.7h46.9c10.2 0 19.9 4.9 25.9 13.3l71.2 98.8 157.2-218c6-8.3 15.6-13.3 25.9-13.3H699c6.5 0 10.3 7.4 6.5 12.7z"></path>
                       </svg>
                       <h1 className="text-justify md:font-bold px-1 basis-1/2">
-                        Смелый запрос
+                        Смелый подход
                       </h1>
                     </div>
                     <div className="pl-3">
                       <a href="/">
                         <h1 className="md:font-normal hover:text-[#FCA311]">
-                          Бесстрашно решаем сложные вопросы.
+                          Решаем даже самые сложные задачи.
                         </h1>
                       </a>
                     </div>
@@ -471,7 +409,7 @@ function WhoWeAre() {
                     <div className="pl-3">
                       <a href="/">
                         <h1 className="font-normal hover:text-[#FCA311]">
-                          Представляем только самых лучших кандидатов.
+                          Рекомендуем только лучших специалистов.
                         </h1>
                       </a>
                     </div>
@@ -496,7 +434,7 @@ function WhoWeAre() {
                     <div className="pl-3">
                       <a href="/">
                         <h1 className="font-normal hover:text-[#FCA311]">
-                          Стремление к долгосрочному сотрудничеству.
+                          Ориентируемся на долгосрочные взаимоотношения.
                         </h1>
                       </a>
                     </div>
@@ -515,7 +453,7 @@ function WhoWeAre() {
                         <path d="M256 38.55c-30.5 0-55 24.52-55 55 0 30.45 24.5 54.95 55 54.95s55-24.5 55-54.95c0-30.48-24.5-55-55-55zM191.3 164.4c-5.7 0-11.1.4-16 1.4-19.9 4-34.1 15.6-43.1 35.4-9.4 20.6-12.1 50.6-5.8 88l6 5.9c13.8 13.8 36.1 21.4 58.6 21.4 21.1 0 42.1-6.7 56-19V179.2c-21-9.8-39.8-14.5-55.7-14.8zm129.4 0c-15.9.3-34.7 5-55.7 14.8v118.3c13.9 12.3 34.9 19 56 19 22.5 0 44.8-7.6 58.6-21.4l6-5.9c6.3-37.4 3.6-67.4-5.8-88-9-19.8-23.2-31.4-43.1-35.4-4.9-1-10.3-1.4-16-1.4zm-209.1 14.4h-.2c-30 .7-55.2 12.1-70.2 32.1-13.3 17.8-19.5 42.9-13.6 76 .9 5.1 2.1 10.5 3.7 16 24.5 18.5 54.3 18.6 78.9.3-9.2-44.8-6.9-81.9 5.6-109.4 2.4-5.3 5.2-10.2 8.4-14.7-2.9-.2-5.8-.3-8.6-.3H111.7c-.1 0-.1-.1-.1 0zm288.8 0c0-.1 0 0-.1 0H396.4c-2.8 0-5.7.1-8.6.3 3.2 4.5 6 9.4 8.4 14.7 12.5 27.5 14.8 64.6 5.6 109.4 24.5 18.3 54.4 18.2 78.9-.3 1.5-5.5 2.8-10.9 3.7-16 5.9-33.1-.3-58.2-13.6-76-15-20-40.2-31.4-70.2-32.1h-.2zM132.8 318.4c6.9 26.1 17.7 54.9 32.9 86.1h58.6l22.7-56.7v-28c-16.4 10-36.3 14.7-56 14.7-20.7 0-41.5-5.2-58.2-16.1zm246.4 0c-16.7 10.9-37.5 16.1-58.2 16.1-19.7 0-39.6-4.7-56-14.7v28l22.7 56.7h58.6c15.2-31.2 26-60 32.9-86.1zm-264.6 3.4c-23 13.9-50.1 16.1-74.5 6.4 9.6 23 24.1 48.5 44.5 76.3h61c-14-29.6-24.2-57.2-31-82.7zm282.8 0c-6.8 25.5-17 53.1-31 82.7h61c20.4-27.8 34.9-53.3 44.5-76.3-24.3 9.7-51.5 7.5-74.5-6.4zM256 373.7l-22.1 55.4 22.1 44.3 22.1-44.3zM91.53 422.5l11.47 46h35.3l-23-23 23-23zm90.27 0l16.6 16.6 6.3 6.4-23 23h51.7l-19.3-38.6 3-7.4zm113.1 0l1.5 3.7 1.5 3.7-19.3 38.6h51.7l-23-23 23-23zm78.9 0l16.6 16.6 6.3 6.4-23 23H409l11.5-46zM160 426.2l-19.3 19.3 19.3 19.3 19.3-19.3zm192 0l-19.3 19.3 19.3 19.3 19.3-19.3z"></path>
                       </svg>
                       <h1 className="text-justify md:font-bold px-1">
-                        Целенаправленная специализация
+                        Целевая экспертиза
                       </h1>
                     </div>
                     <div className="pl-3">
@@ -542,14 +480,14 @@ function WhoWeAre() {
               <div className="grid">
                 <h1 className="mt-40"></h1>
                 <h1 className="text-[#fca311] tracking-wider text-justify capitalize font-bold">
-                  04. Бесшовная обработка
+                  04. Беспроблемная адаптация
                 </h1>
                 <span className="border-t-2"></span>
                 <h1 className="text-white text-sm tracking-widest justify-center">
-                  После собеседования мы берем на себя весь процесс - от отзывов
-                  и предложений до ссылок. Мы займемся тонкостями, освобождая
-                  ваше время, чтобы сосредоточиться на предметах первой
-                  необходимости.
+                  После собеседований мы берем на себя весь процесс — от сбора
+                  отзывов и подготовки предложений до проверки рекомендаций. Мы
+                  заботимся о всех деталях, освобождая ваше время для решения
+                  ключевых задач.
                 </h1>
                 <div className="flex justify-center rotate-180">
                   <img
@@ -571,8 +509,9 @@ function WhoWeAre() {
                 <h1 className="border-b-2 mt-10"></h1>
                 <div className="grid place-content-center">
                   <h1 className="py-6 tracking-widest text-white text-sm">
-                    Погрузиться в вашу компанию, культуру, ценности и роль
-                    требования для полного понимания ваших надежд.
+                    Мы глубоко погружаемся в вашу компанию, её культуру,
+                    ценности и требования к роли, чтобы полностью понять ваши
+                    ожидания.
                   </h1>
                 </div>
                 <div className="grid place-content-center mx-auto">
@@ -590,13 +529,13 @@ function WhoWeAre() {
                     />
                   </div>
                   <h1 className="text-[#fca311] tracking-wider font-bold mt-24 capitalize">
-                    03. Тщательная оценка
+                    03. Детальная оценка
                   </h1>
                   <h1 className="border-b-2 pt-6"></h1>
                   <h1 className="py-6 tracking-widest text-white text-sm justify-center">
-                    Тщательно проанализировать, составить короткий список и
-                    представить наиболее актуальных кандидатов, упрощая путь к
-                    этапу собеседования.
+                    Мы проводим тщательный анализ, составляем короткий список и
+                    представляем вам наиболее подходящих кандидатов, чтобы
+                    упростить процесс собеседования.
                   </h1>
                 </div>
               </div>
@@ -614,13 +553,13 @@ function WhoWeAre() {
                   />
                 </div>
                 <h1 className="pt-16 text-[#fca311] font-bold capitalize">
-                  02. Целевое ресурсное обеспечение
+                  02. Целенаправленный поиск
                 </h1>
                 <h1 className="border-b-2"></h1>
                 <h1 className="pt-10 text-sm text-white tracking-widest">
-                  Обратитесь к нашим консультантам; понимание отрасли,
-                  осведомленность о наличие кандидатов и обширная сеть для того
-                  чтобы тщательно подобрать наиболее подходящие профили.
+                  Наши консультанты, обладая глубокими знаниями отрасли и
+                  широкой сетью контактов, тщательно подбирают наиболее
+                  подходящие кандидатуры.
                 </h1>
                 <div className="flex justify-center rotate-90">
                   <img
@@ -643,45 +582,46 @@ function WhoWeAre() {
                 </h1>
                 <div className="grid place-content-center py-2 border-t-[#fca311]">
                   <h1 className="py-2 px-6 tracking-widest text-white text-sm">
-                    Погрузиться в вашу компанию, культуру, ценности и роль
-                    требования для полного понимания ваших надежд.
+                    Мы глубоко погружаемся в вашу компанию, её культуру,
+                    ценности и требования к роли, чтобы полностью понять ваши
+                    ожидания.
                   </h1>
                 </div>
               </div>
               <div className="px-2">
                 <h1 className="text-[#fca311] tracking-wider font-bold capitalize">
-                  02. Целевое ресурсное обеспечение
+                  02. Целенаправленный поиск
                 </h1>
                 <div className="grid place-content-center py-2 border-t-[#fca311]">
                   <h1 className="py-2 px-6 tracking-widest text-white text-sm">
-                    Обратитесь к нашим консультантам; понимание отрасли,
-                    осведомленность о наличие кандидатов и обширная сеть для
-                    того чтобы тщательно подобрать наиболее подходящие профили.
+                    Наши консультанты, обладая глубокими знаниями отрасли и
+                    широкой сетью контактов, тщательно подбирают наиболее
+                    подходящие кандидатуры.
                   </h1>
                 </div>
               </div>
               <div className="px-2">
                 <h1 className="text-[#fca311] tracking-wider font-bold capitalize">
-                  03. Тщательная оценка
+                  03. Детальная оценка
                 </h1>
                 <div className="grid place-content-center py-2 border-t-[#fca311]">
                   <h1 className="py-2 px-6 tracking-widest text-white text-sm">
-                    Тщательно проанализировать, составить короткий список и
-                    представить наиболее актуальных кандидатов, упрощая путь к
-                    этапу собеседования.
+                    Мы проводим тщательный анализ, составляем короткий список и
+                    представляем вам наиболее подходящих кандидатов, чтобы
+                    упростить процесс собеседования.
                   </h1>
                 </div>
               </div>
               <div className="px-2">
                 <h1 className="text-[#fca311] tracking-wider font-bold capitalize">
-                  04. Бесшовная обработка
+                  04. Беспроблемная адаптация
                 </h1>
                 <div className="grid place-content-center py-2 border-t-[#fca311]">
                   <h1 className="py-2 px-6 tracking-widest text-white text-sm">
-                    После собеседования мы берем на себя весь процесс - от
-                    отзывов и предложений до ссылок. Мы займемся тонкостями,
-                    освобождая ваше время, чтобы сосредоточиться на предметах
-                    первой необходимости.
+                    После собеседований мы берем на себя весь процесс — от сбора
+                    отзывов и подготовки предложений до проверки рекомендаций.
+                    Мы заботимся о всех деталях, освобождая ваше время для
+                    решения ключевых задач.
                   </h1>
                 </div>
               </div>
